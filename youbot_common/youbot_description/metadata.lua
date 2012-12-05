a349b8ea15ddc1a70b68d5bfdb7e12c1289cab80
DominickVanthienen <dominick.vanthienen@mech.kuleuven.be>
2012-12-05 18:00:18 +0100

    adding metadata

---
+++ youbot_common/youbot_description/metadata.lua
@@ -0,0 +1,6 @@
+{ports={
+  JointVelocitiesCommand={type='driver', geom_sem='JointVelocitySemantics(ee, base)', topic='/JointVelocitiesCommand', direction='input'},
+  base_controller_command={type='driver', geom_sem='TwistCoordinateSemantics(ee,ee,b,b)', topic='/base_controller/command', direction='input'},
+  joint_states_translated={type='driver', geom_sem='JointPositionSemantics(ee, base)', topic='/joint_states_translated', direction='output'},
+  base_odometry_odom={type='driver', geom_sem='JointPositionSemantics(base,b)', topic='/base_odometry/odom', direction='output'}
+}}
