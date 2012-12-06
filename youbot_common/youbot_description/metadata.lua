9f8bfdafc37f117c839ca1f492861f318d4fb092
DominickVanthienen <dominick.vanthienen@mech.kuleuven.be>
2012-12-06 10:47:37 +0100

    making metadata return

--- youbot_common/youbot_description/metadata.lua
+++ youbot_common/youbot_description/metadata.lua
@@ -1,4 +1,4 @@
-{ports={
+return {ports={
   JointVelocitiesCommand={type='driver', geom_sem='JointVelocitySemantics(ee, base)', topic='/JointVelocitiesCommand', direction='input'},
   base_controller_command={type='driver', geom_sem='TwistCoordinateSemantics(ee,ee,b,b)', topic='/base_controller/command', direction='input'},
   joint_states_translated={type='driver', geom_sem='JointPositionSemantics(ee, base)', topic='/joint_states_translated', direction='output'},
