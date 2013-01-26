5f13160acced88e74c106827882bba453f2df1df
DominickVanthienen <dominick.vanthienen@mech.kuleuven.be>
2013-01-26 16:31:30 +0100

    adding framework option

--- youbot_common/youbot_description/metadata.lua
+++ youbot_common/youbot_description/metadata.lua
@@ -1,4 +1,6 @@
-return {ports={
+return {
+framework={"ROS"},
+ports={
   JointVelocitiesCommand={rtt_type='/motion_control_msgs/JointVelocities', type='driver', geom_sem='JointVelocitySemantics(ee,base)', topic='/JointVelocitiesCommand', direction='input'},
   base_controller_command={rtt_type='/geometry_msgs/Twist', type='driver', geom_sem='TwistCoordinateSemantics(ee,ee,b,b)', topic='/base_controller/command', direction='input'},
   joint_states_translated={rtt_type='/sensor_msgs/JointState', type='driver', geom_sem='JointPositionSemantics(ee,base)', topic='/joint_states_translated', direction='output'},
